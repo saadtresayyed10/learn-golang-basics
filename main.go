b3b67ae0e30392e25b325e67934aff7a320fefa1
Saad Sayyed <saadsyed950@gmail.com>
2024-10-27 15:05:22 +0530

    Arrays done

--- main.go
+++ main.go
@@ -13,10 +13,12 @@ func main() {
 	fmt.Printf("We have total of %v tickets from which %v are remaining\n", conferenceTickets, remainingTickets)
 	fmt.Printf("Get your tickets to attend\n")
 
+	var bookings [50]string
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter the first name: ")
 	fmt.Scan(&firstName)
@@ -30,6 +32,14 @@ func main() {
 	fmt.Println("Enter the no. of tickets to buy: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("User %v %v has bought %v tickets using %v email", firstName, lastName, userTickets, email)
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Println("Whole array: ", bookings)
+	fmt.Println("1st element in array: ", bookings[0])
+	fmt.Println("Size of the array: ", len(bookings))
+
+	fmt.Printf("User %v %v has bought %v tickets using %v email\n", firstName, lastName, userTickets, email)
+	fmt.Printf("Total remaining tickets %v\n", remainingTickets)
 
 }
