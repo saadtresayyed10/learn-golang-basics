8222703954754c0700a0c719c2b0480ba7ddbc61
Saad Sayyed <saadsyed950@gmail.com>
2024-10-27 14:26:29 +0530

    Datatypes done

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Conference type %T, Conference Tickets type %T\n", conferenceName, conferenceTickets)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets from which %v are remaining\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets to attend")
+	fmt.Printf("Get your tickets to attend\n")
+
+	var username string
+	var userTickets int
+
+	username = "Saad"
+	userTickets = 2
+
+	fmt.Printf("User %v has booked %v tickets\n", username, userTickets)
+
 }
